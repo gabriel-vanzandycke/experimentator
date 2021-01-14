295fff0a02199381e197cb502cf33c3475c3cdf0
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-01-14 16:42:45 +0000

    remove ambiguous import tf1 vs tf2

--- experimentator/__init__.py
+++ experimentator/__init__.py
@@ -2,11 +2,4 @@ import utils
 from .base_experiment import BaseExperiment, LoggedExperiment
 from .keys_splitter import KeysSplitter, Subset
 from .manager import ExperimentManager, ExperimentManagerNotebook
-try:
-    from .tf1_experiment import TensorflowExperiment
-except BaseException as e:
-    # TODO: tensorflow missing module exception should be handeld if user doesn't want to use tensorflow (1.14).
-    raise e
-from . import tf1_chunk_processors as chunk_processors
-from .tf1_chunk_processors import ChunkProcessor
 from .callbacks import Callback
