17bdf51d5c9acd050c9982e8ce46b5de018d7efb
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-01-26 11:14:02 +0000

    gpu argument not required (defaults to 0)

--- experimentator/app.py
+++ experimentator/app.py
@@ -9,7 +9,8 @@ def run():
     parser = argparse.ArgumentParser()
     parser.add_argument("--epochs", type=int, default=-1, help="Number of epochs to train (if not provided or 0, will prompt)")
     parser.add_argument("--config", required=True, help="Experiment '.ini' configuration file. If no config is provided, prompts to loads a previous process")
-    parser.add_argument("--gpu", required=True, help="Default GPU to use")
+    parser.add_argument("--gpu", default=0, help="Default GPU to use")
+    parser.add_argument("--cpu", action="store_true", help="Default GPU to use")
     parser.add_argument("--robust", action="store_true", help="Should experiments be encapsulated inside a try-except")
     parser.add_argument("--name", default=None, help="Config file name")
     parser.add_argument("--eager", action="store_true", help="should be run in eagermode")
@@ -17,6 +18,8 @@ def run():
 
     args = parser.parse_args()
     
+    #assert args.gpu is not None or args.cpu, "You must specify either cpu or gpu"
+
     try:
         import provision#from provision import *
     except BaseException as e:
