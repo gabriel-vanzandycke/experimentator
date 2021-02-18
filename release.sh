2857e858a95078fa460b62db285f76cd105c31c6
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-02-18 12:33:22 +0000

    add release script

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Extract version number from setup.py
+RELEASE_NUM=`grep version setup.py | cut -d\" -f2 | cut -d\' -f2`
+echo "RELEASE_NUM=$RELEASE_NUM"
+
+# Tag in Git and push to remote
+git tag $RELEASE_NUM -m "Tagging release $RELEASE_NUM"
+git push --tags
+
