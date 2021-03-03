be140fea4a2159e784511982e46654c5cc921f86
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-03-03 12:54:30 +0000

    increase version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     licence="LGPL",
     python_requires='>=3.6',
     description="My deep-learning experimentation framework",
-    version='1.2.0',
+    version='1.3.0',
     packages=find_packages(),
     install_requires=[
         "mlworkflow",
