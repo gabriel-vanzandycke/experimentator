8b8880ac49e00185315d3dde1c99aa3713567886
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-04-07 13:51:15 +0000

    use generic logger callback in wandb

--- experimentator/wandb_experiment.py
+++ experimentator/wandb_experiment.py
@@ -1,39 +1,29 @@
 import os
-import json
 import pandas
-import logging
+from mlworkflow import lazyproperty
 import wandb
-from .callbacked_experiment import Callback
+from .logger_experiment import LoggerCallback
 
-class LogStateWandB(Callback):
-    precedence = 100 # very last
-    state = {}
-    def __init__(self, **d):
-        self.d = d
-    def init(self, exp): # pylint: disable=super-init-not-called
-        os.environ["WANDB_SILENT"] = "true"
-        project_name = exp.get("project_name", "unknown_project")
-        grid_sample = dict(exp.grid_sample) # copies the original dictionary
-        grid_sample.pop("fold", None)       # removes 'fold' to be able to group runs
-        run_name = json.dumps(grid_sample)
+os.environ["WANDB_SILENT"] = "true"
+
+class LogStateWandB(LoggerCallback):
+    @lazyproperty
+    def wandb(self):
         wandb.init(
-            project=project_name,
+            project=self.project_name,
             reinit=True,
-            config={k:str(v) for k,v in exp.cfg.items()},
+            config=self.config,
             settings=wandb.Settings(show_emoji=False, show_info=False, show_warnings=False)
         )
-        wandb.run.name = run_name
+        wandb.run.name = self.run_name
+        return wandb
     def __del__(self):
-        wandb.finish()
-    def on_epoch_begin(self, **_):
-        self.state = {}
-    def on_cycle_end(self, subset, state, **_):
-        self.state.update({subset + "_" + k: v for k,v in state.items()})
+        self.wandb.finish()
     def on_epoch_end(self, **_):
         report = {}
         for key, data in self.state.items():
             if isinstance(data, pandas.DataFrame):
-                report[key] = wandb.Table(dataframe=data)
+                report[key] = self.wandb.Table(dataframe=data)
             else:
                 report[key] = data
-        wandb.log(report) # log *once* per epoch
+        self.wandb.log(report) # log *once* per epoch
