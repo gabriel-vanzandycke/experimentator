3df58d2eb70ec6aeef62a2c0e46e34073df70c08
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-04-10 17:07:59 +0000

    robust neptune dependancy

--- experimentator/__init__.py
+++ experimentator/__init__.py
@@ -5,7 +5,10 @@ from .keys_splitter import KeysSplitter, Subset
 from .manager import ExperimentManager#, ExperimentManagerNotebook
 from .callbacked_experiment import Callback, CallbackedExperiment, InitState, MeasureTime, AverageLoss, AccumulateBatchMetrics
 from .wandb_experiment import LogStateWandB
-from .neptune_experiment import LogStateNeptune
 from .tf2_experiment import TensorflowExperiment
 from .tf2_chunk_processors import ChunkProcessor
 from .logging import LoggingExperiment
+try:
+    from .neptune_experiment import LogStateNeptune
+except ModuleNotFoundError:
+    pass
