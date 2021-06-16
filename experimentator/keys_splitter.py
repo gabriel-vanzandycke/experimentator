3e7b356e494731f31f82a4f082d784288d9b7ae6
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-06-16 09:39:04 +0000

    add basic keys splitter in experimentator

--- experimentator/keys_splitter.py
+++ experimentator/keys_splitter.py
@@ -26,3 +26,36 @@ class Subset():
 class KeysSplitter(): # pylint: disable=too-few-public-methods
     def __str__(self):
         return self.__class__.__name__
+
+
+class BasicKeysSplitter(KeysSplitter):
+    def __init__(self, validation_pc=15, testing_pc=15):
+        assert validation_pc + testing_pc < 100
+        self.validation_pc = validation_pc
+        self.testing_pc = testing_pc
+
+    def __call__(self, keys, fold=0):
+        keys = list(keys.all())
+        l = len(keys)
+
+        # Backup random seed
+        random_state = random.getstate()
+        random.seed(fold)
+
+        random.shuffle(keys)
+
+        # Restore random seed
+        random.setstate(random_state)
+
+        u1 = self.validation_pc
+        u2 = self.validation_pc + self.testing_pc
+
+        validation_keys = keys[00*l//100:u1*l//100]
+        testing_keys    = keys[u1*l//100:u2*l//100]
+        training_keys   = keys[u2*l//100:]
+
+        return {
+            "training": Subset("training", "TRAIN", training_keys),
+            "validation": Subset("validation", "VALID", validation_keys, repetitions=5),
+            "testing": Subset("testing", "TEST", testing_keys, repetitions=5),
+        }
