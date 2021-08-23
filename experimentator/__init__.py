10ed3ee20cf29ee88a8a2c2ba16ebfaef86dfc84
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-08-23 14:16:59 +0000

    wandb changes

--- experimentator/__init__.py
+++ experimentator/__init__.py
@@ -2,7 +2,7 @@ import utils
 from .utils import ExperimentMode, ChunkProcessor, DataCollector
 from .base_experiment import BaseExperiment, AsyncExperiment, DummyExperiment
 from .dataset import DatasetSplitter, BasicDatasetSplitter, SubsetType, Subset
-from .manager import ExperimentManager, parse_config_str, parse_config_file#, ExperimentManagerNotebook
+from .manager import ExperimentManager, parse_config_str, parse_config_file
 from .callbacked_experiment import Callback, CallbackedExperiment, MeasureTime, StopFailedTraining, \
     AccumulateBatchMetrics, SaveLearningRate,  SaveWeights, StateLogger, LogStateDataCollector, GatherCycleMetrics, \
     AverageMetrics, LearningRateDecay, LearningRateWarmUp, PrintLoss
--- experimentator/wandb_experiment.py
+++ experimentator/wandb_experiment.py
@@ -2,6 +2,7 @@ import os
 import pandas
 from mlworkflow import lazyproperty
 from experimentator import StateLogger
+import wandb
 
 os.environ["WANDB_SILENT"] = "true"
 os.environ["WANDB_START_METHOD"] = "thread"
@@ -13,33 +14,32 @@ class LogStateWandB(StateLogger):
         self.mode = mode
         self.initialized = False
     @lazyproperty
-    def wandb(self):
-        import wandb
-        wandb.init(
+    def wandb_run(self):
+        run = wandb.init(
             project=self.project_name,
             reinit=True,
             config=self.config,
             settings=wandb.Settings(show_emoji=False),
             mode=self.mode,
         )
-        wandb.run.name = self.run_name
+        run.name = self.run_name
         self.initialized = True
-        return wandb
+        return run
     def __del__(self):
-        if self.initialized:
-            self.wandb.finish()
+        if self.initialized
+            wandb.finish()
     def on_epoch_end(self, state, **_):
         report = {}
         for key, data in state.items():
             if key not in self.excluded_keys:
                 if isinstance(data, pandas.DataFrame):
-                    report[key] = self.wandb.Table(dataframe=data)
+                    report[key] = wandb.Table(dataframe=data)
                 else:
                     report[key] = data
-        self.wandb.log(report) # log *once* per epoch
+        self.wandb_run.log(report) # log *once* per epoch
 
         if self.criterion_metric and self.criterion_metric in report:
             if not self.best_report or report[self.criterion_metric] > self.best_report[self.criterion_metric]:
                 self.best_report = report
-            self.wandb.run.summary.update(self.best_report)
+            self.wandb_run.summary.update(self.best_report)
 
