b8acda5046fca732ba59a767e5759368b845f6dc
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-10-01 11:02:26 +0000

    increase required python version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author_email="gabriel.vanzandycke@hotmail.com",
     url="https://github.com/gabriel-vanzandycke/experimentator",
     licence="LGPL",
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     description="My deep-learning experimentation framework",
     version='2.1.0',
     packages=find_packages(),
