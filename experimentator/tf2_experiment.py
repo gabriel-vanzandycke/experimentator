f60d0d4b20badbcc1bb17181112de60cdfbafa57
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-10-18 15:56:23 +0000

    input specs settable in tensorflow2 experiment

--- experimentator/tf2_experiment.py
+++ experimentator/tf2_experiment.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass
 from enum import IntFlag
-from mlworkflow import lazyproperty as cached_property
+from functools import cached_property
 import glob
 import logging
 import os
 
-import numpy as np
 import tensorflow as tf
 from tensorflow.python.client import timeline # pylint: disable=no-name-in-module, unused-import
 
@@ -24,6 +23,8 @@ class TensorFlowModelWrapper(tf.keras.Model): # pylint: disable=abstract-method
     def train_step(self, inputs):
         with tf.GradientTape() as tape:
             results = self(inputs, training=True)
+        if tf.math.reduce_any(tf.math.is_nan(results["loss"])):
+            pass #return results
         grads = tape.gradient(results["loss"], self.trainable_weights)
         self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
         return results
@@ -60,8 +61,8 @@ class TensorflowExperiment(BaseExperiment):
         return self.cfg["optimizer"]
 
     def load_weights(self, filename="auto"):
-        if filename == "auto":
-            dirname = os.path.dirname(self.cfg["filename"])
+        if filename == "auto" or os.path.isdir(filename):
+            dirname = os.path.dirname(self.cfg["filename"]) if filename == "auto" else filename
             try:
                 filename = sorted(glob.glob(os.path.join(dirname, "*.index")))[-1].replace(".index", "")
             except IndexError:
@@ -88,32 +89,19 @@ class TensorflowExperiment(BaseExperiment):
     #     checkpoint_dir = os.path.join()
     #     tf.train.CheckpointManager(self.checkpoint, )
 
-    def __init_inputs(self, data):
+    @cached_property
+    def inputs_specs(self): #pylint: disable=method-hidden
+        data = next(iter(self.batch_generator(subset=self.subsets['training'])))[1]
+        return self.inputs_specs_from_batch(data)
+
+    def init_inputs(self, inputs_specs):
         if "inputs" not in self.__dict__:
-            self.__data = data
+            self.inputs_specs = inputs_specs
             _ = self.inputs
-            del self.__data
 
     @cached_property
     def inputs(self):
-        try:
-            data = self.__data # temporary variable holding the dictionary of batched data
-        except AttributeError:
-            data = next(iter(self.batch_generator(subset=self.subsets['training'])))[1]
-        inputs = {}
-        skipped = []
-        for tensor_name in data:
-            if isinstance(data[tensor_name], (np.ndarray, np.int32, int, float)):
-                inputs[tensor_name] = tf.keras.Input(
-                    dtype=tf.dtypes.as_dtype(data[tensor_name].dtype),
-                    shape=data[tensor_name].shape[1:], # removing batch dimension in tf.keras.Input
-                    name=tensor_name
-                )
-            else:
-                skipped.append(tensor_name)
-        if skipped:
-            self.logger.debug("Skipped inputs: " + ", ".join(["{}({})".format(name, data[name]) for name in skipped]))
-        return inputs
+        return {name: tf.keras.Input(type_spec=type_spec, name=name) for name, type_spec in self.inputs_specs.items()}
 
     @cached_property
     def chunk_processors(self):
@@ -159,17 +147,21 @@ class TensorflowExperiment(BaseExperiment):
     def select_data(self, data):
         return {k:v for k,v in data.items() if k in self.inputs}
 
+    @staticmethod
+    def inputs_specs_from_batch(data):
+        return {name: tf.TensorSpec(dtype=array.dtype, shape=array.shape) for name, array in data.items()}
+
     def batch_train(self, data, mode=ExperimentMode.TRAIN):
-        self.__init_inputs(data)
+        self.init_inputs(self.inputs_specs_from_batch(data))
         model = self.train_model if mode & ExperimentMode.TRAIN else self.eval_model
         return model.train_step(self.select_data(data))
 
     def batch_eval(self, data):
-        self.__init_inputs(data)
+        self.init_inputs(self.inputs_specs_from_batch(data))
         return self.eval_model.test_step(self.select_data(data))
 
     def batch_infer(self, data):
-        self.__init_inputs(data)
+        self.init_inputs(self.inputs_specs_from_batch(data))
         return self.infer_model.test_step(self.select_data(data))
 
 
@@ -208,137 +200,3 @@ class ProfileCallback(Callback):
         if batch == self.batch_stop:
             tf.profiler.experimental.stop()
             print("="*100 + "\nStop profiling\n" + "="*100)
-
-
-
-
-
-
-# class TensorFlowProfilerExperiment(TensorflowExperiment):
-#     def run_batch(self, subset, batch_id, *args, **kwargs):
-#         with tf.profiler.experimental.Trace(subset.name, step_num=batch_id, _r=1):
-#             result = super().run_batch(*args, subset=subset, batch_id=batch_id, **kwargs)
-#         return result
-
-
-# @dataclass
-# class TraceCallback(Callback):
-#     logdir: str = "logdir"
-#     def __post_init__(self):
-#         self.writer = tf.summary.create_file_writer(self.logdir)
-#         tf.debugging.experimental.enable_dump_debug_info(self.logdir, tensor_debug_mode="FULL_HEALTH", circular_buffer_size=-1)
-#     def on_batch_begin(self, batch, **_):
-#         if 2 == batch:
-#             tf.summary.trace_on(graph=True, profiler=True)
-#     def on_batch_end(self, batch, epoch, **_):
-#         if batch == 10:
-#             with self.writer.as_default():
-#                 tf.summary.trace_export(name="on_batch_end_{}.{}".format(epoch, batch), step=0, profiler_outdir=self.logdir)
-
-#     def freeze(self, filename, output_nodes_names=None):
-#         """Freezes the state of a session into a pruned computation graph.
-#         Creates a new computation graph where variable nodes are replaced by
-#         constants taking their current value in the session. The new graph will be
-#         pruned so subgraphs that are not necessary to compute the requested
-#         outputs are removed.
-#         @param session The TensorFlow session to be frozen.
-#         @param keep_var_names A list of variable names that should not be frozen,
-#                             or None to freeze all the variables in the graph.
-#         @param output_names Names of the relevant graph outputs.
-#         @param clear_devices
-#         @return The frozen graph definition.
-#         """
-#         assert filename[-3:] == ".pb", "You must provide a *.pb file"
-#         output_nodes_names = output_nodes_names or list(self.outputs.keys())
-#         # Remove the device directives from the graph for better portability
-#         graph_definition = self.graph.as_graph_def()
-#         for node in graph_definition.node: # pylint: disable=no-member
-#             node.device = ""
-#         frozen_graph = tf.compat.v1.graph_util.convert_variables_to_constants(self.session, graph_definition, output_nodes_names)
-#         tf.train.write_graph(frozen_graph, os.path.dirname(filename), os.path.basename(filename), as_text=False)
-
-#         # compute the number of flops
-#         opt = tf.profiler.ProfileOptionBuilder.float_operation()
-#         flops = tf.profiler.profile(self.graph, options=opt)
-#         total_flops = flops.total_float_ops
-#         print("frozen graph saved in '{}' with {} flops".format(filename, total_flops))
-
-
-
-
-# class ProfileBatch(Callback):
-#     precedence = 95 # almost last
-#     def __init__(self, exp):
-#         self.loggdir = exp.get("loggdir", "profiler_logs")
-#         self.profiler = tf.profiler.Profiler(graph=exp.graph)
-#         self.exp = exp
-#         self.summary = tf.summary.FileWriter(self.loggdir + '/train', graph=exp.graph, session=exp.session)
-#         self.target_batch = 10
-
-#     def on_batch_begin(self, batch_id, **_):
-#         if batch_id != self.target_batch:
-#             return
-#         #tf.summary.trace_on(graph=True, profiler=True)
-#         self.exp.run_metadata = tf.RunMetadata()
-#         self.exp.run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE) # pylint: disable=no-member
-#         # tf.profiler.experimental.start(
-#         #     self.loggdir, tf.profiler.ProfilerOptions(host_tracer_level=2)
-#         # )
-#     def on_batch_end(self, batch_id, subset, **_):
-#         key = "{}_{}_{}".format(self.exp["experiment_id"], subset, batch_id)
-#         if batch_id != self.target_batch:
-#             return
-
-#         if False:
-#             # with self.summary.as_default():
-#             #     for metric in self.exp.metrics.keys():
-#             #         tf.summary.scalar(metric, state[metric], step=batch_id)
-#             #     tf.summary.trace_export(
-#             #         name="func_trace",
-#             #         step=batch_id,
-#             #         profiler_outdir=self.loggdir
-#             #     )
-#             self.summary.add_run_metadata(self.exp.run_metadata, "run_metadata", batch_id)
-#             # for metric_name in self.exp.metrics.keys():
-#             #     tf.summary.scalar(metric_name, state[metric_name])
-#             #     self.summary.add_summary()#, step=batch_id))
-
-#             self.profiler.add_step(batch_id, self.exp.run_metadata)
-
-
-#             #self.profiler.profile_operations(opts)
-
-#             # opts = (tf.profiler.ProfileOptionBuilder()
-#             #     .with_max_depth(10)
-#             #     .with_min_micros(1000)
-#             #     .select(['accelerator_micros'])
-#             #     .with_stdout_output()
-#             #     .build())
-#             #     #.time_and_memory()
-#             #     #.with_stdout_output()
-#             #     #.build())
-
-#             # Or you can generate a timeline:
-
-#         opts = (tf.profiler.ProfileOptionBuilder(
-#                 tf.profiler.ProfileOptionBuilder.time_and_memory())
-#                 .with_step(batch_id)
-#                 .with_timeline_output("{}_timeline.ctf".format(key)).build())
-#         self.profiler.profile_graph(options=opts)
-
-#         if True:
-#             opts = (tf.profiler.ProfileOptionBuilder(
-#                     tf.profiler.ProfileOptionBuilder.time_and_memory())
-#                     .with_step(batch_id)
-#                     .with_timeline_output("{}_timeline_python.ctf".format(key)).build())
-#             self.profiler.profile_python(options=opts)
-#             #tf.profiler.experimental.stop()
-#         #
-#         #
-#         if True:
-#             tl = timeline.Timeline(self.exp.run_metadata.step_stats) # pylint: disable=no-member
-#             with open("tf_timeline_{}".format(key), 'w') as f:
-#                 f.write(tl.generate_chrome_trace_format())
-
-#         self.exp.run_metadata = None
-#         self.exp.run_options = None
