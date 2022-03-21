6c2a5ceef3f106c595863a66e88ad642b091d62d
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2022-03-21 15:14:16 +0000

    avoid relative imports

--- experimentator/__init__.py
+++ experimentator/__init__.py
@@ -1,9 +1,9 @@
 import utils
-from .utils import ExperimentMode, ChunkProcessor, DataCollector
-from .base_experiment import BaseExperiment, AsyncExperiment, DummyExperiment
-from .dataset import BasicDatasetSplitter, SubsetType, Subset
-from .manager import ExperimentManager, parse_config_str, parse_config_file, build_experiment
-from .callbacked_experiment import Callback, CallbackedExperiment, MeasureTime, StopFailedTraining, \
+from experimentator.utils import ExperimentMode, ChunkProcessor, DataCollector
+from experimentator.base_experiment import BaseExperiment, AsyncExperiment, DummyExperiment
+from experimentator.dataset import BasicDatasetSplitter, SubsetType, Subset
+from experimentator.manager import ExperimentManager, parse_config_file, build_experiment
+from experimentator.callbacked_experiment import Callback, CallbackedExperiment, MeasureTime, StopFailedTraining, \
     SaveLearningRate,  SaveWeights, StateLogger, LogStateDataCollector, GatherCycleMetrics, \
     AverageMetrics, LearningRateDecay, LearningRateWarmUp, PrintLoss, LoadWeights, FindLearningRate
-from .loggers import LoggingExperiment
+from experimentator.loggers import LoggingExperiment
