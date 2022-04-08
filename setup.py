99b035be3a7f04a5d1c9e9aab1417aa9176bb971
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2022-04-08 10:05:45 +0000

    increase version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     licence="LGPL",
     python_requires='>=3.8',
     description="My deep-learning experimentation framework",
-    version='2.2.6',
+    version='2.2.7',
     packages=find_packages(),
     entry_points={
         'console_scripts': ['experiment=experimentator:main'],
