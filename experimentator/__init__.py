585daf89e725f0bdd99e0be13ae20be7d63d05c8
Gabriel Van Zandycke <gabriel.vanzandycke@hotmail.com>
2023-04-13 15:12:46 +0200

    add cached pickleddataset into module

--- experimentator/__init__.py
+++ experimentator/__init__.py
@@ -1,8 +1,8 @@
 from experimentator.utils import ExperimentMode, ChunkProcessor, DataCollector, ConfusionMatrix
-from experimentator.dataset import find, BasicDatasetSplitter, SubsetType, Subset, collate_fn, CombinedSubset
+from experimentator.dataset import find, BasicDatasetSplitter, SubsetType, Subset, collate_fn, CombinedSubset, CachedPickledDataset
 from experimentator.base_experiment import BaseExperiment, AsyncExperiment, DummyExperiment
 from experimentator.manager import ExperimentManager, parse_config_file, build_experiment
-from experimentator.callbacked_experiment import Callback, CallbackedExperiment, MeasureTime, StopFailedTraining, \
+from experimentator.callbacked_experiment import Callback, CallbackedExperiment, MeasureTime, \
     SaveLearningRate,  SaveWeights, StateLogger, LogStateDataCollector, GatherCycleMetrics, \
     AverageMetrics, LearningRateDecay, LearningRateWarmUp, PrintLoss, LoadWeights, FindLearningRate
 from experimentator.loggers import LoggingExperiment
